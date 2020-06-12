4c32793cb91d2016513cde0d748b7b95e16de2b6
Brandon Koenning <brandonkoenning@Brandons-MBP.austin.rr.com>
2020-06-12 18:58:08 -0500

    first commit

---
+++ pack.php
@@ -0,0 +1,17 @@
+<?php
+	$fh = fopen("package.txt", 'r') or die('Error on file open');
+	$str0 = fgets($fh);
+	//print($str0);
+
+	$keys = explode("\t", $str0);
+
+	foreach ($keys as $value) {
+		# code...
+		print($value);
+		
+	}
+
+
+
+
+?>
